bd0d9a0162d75c91ee4fa9262f99f1d73b2b8da1
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-01-14 11:18:44 +0800

    KernelSU: fs, drivers: scope-minimized manual hooks k3.0 v1.9 - hybrid
    This refactors original KSU hooks to replace deep kernel function hooks with targeted hooks.
    This backports KernelSU pr#1657 and having pr#2084 elements (32-bit sucompat).
    It reduces the scope of kernel function interception and still maintains full fucntionality.
    
    https://github.com/backslashxx/KernelSU/issues/5
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1488,6 +1488,11 @@ EXPORT_SYMBOL(search_binary_handler);
 /*
  * sys_execve() executes a new program.
  */
+#ifdef CONFIG_KSU
+__attribute__((hot)) 
+extern int ksu_legacy_execve_sucompat(const char **filename_ptr, void *argv, void *envp);
+#endif
+
 static int do_execve_common(const char *filename,
 				struct user_arg_ptr argv,
 				struct user_arg_ptr envp,
@@ -1517,6 +1522,9 @@ static int do_execve_common(const char *filename,
 	 * further execve() calls fail. */
 	current->flags &= ~PF_NPROC_EXCEEDED;
 
+#ifdef CONFIG_KSU
+	ksu_legacy_execve_sucompat(&filename, &argv, &envp);
+#endif
 	retval = unshare_files(&displaced);
 	if (retval)
 		goto out_ret;
